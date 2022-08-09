5bbc2fe129fadb5c6efb0dfeece16d2f55d55121
Laurits Lund Støvring <laur887b@elev.techcollege.dk>
2022-08-09 13:23:58 +0200

    Big mistake

---
+++ Pages/OmMig - Copy (2).cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio_Web_App.Pages
+{
+    public class OmMigModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Projekter.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio_Web_App.Pages
+{
+    public class ProjekterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
