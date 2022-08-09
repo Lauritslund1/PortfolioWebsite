7f9f44e554c87a64bc65cdd6c71e9d226c0cf25b
laur8 <laur8@BASED-PC2>
2022-08-09 11:14:01 +0200

    Add project files.

---
+++ Pages/Page2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio_Web_App.Pages
+{
+    public class Page2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
