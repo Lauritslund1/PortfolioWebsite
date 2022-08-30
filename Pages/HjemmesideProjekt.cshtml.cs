d406d8db15c3ab715890dcfd43dafcc110965554
Laurits Lund Støvring <laur887b@elev.techcollege.dk>
2022-08-30 09:52:14 +0200

    Oprettet undersider til Projekterne, og lavet småjusteringer

---
+++ Pages/HjemmesideProjekt.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio_Web_App.Pages
+{
+    public class HjemmesideProjektModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/VinLagerSystem.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Portfolio_Web_App.Pages
+{
+    public class VinLagerSystemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
